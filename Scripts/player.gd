9a7a5ef1b1ca7c1c6c5f407f4be46be6653075ef
Skylar111111 <ludvigsenjack1@gmail.com>
2026-03-19 17:36:41 +1300

    player added

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 50
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(_delta):
 	var input_direction = Vector2(
@@ -10,3 +11,23 @@ func _physics_process(_delta):
 
 	velocity = input_direction * move_speed
 	move_and_slide()
+
+	var Xdirection := Input.get_axis("left", "right")
+	
+	var Ydirection := Input.get_axis("up", "down")
+	
+	if Xdirection > 0:
+		animated_sprite.flip_h = false
+	elif Xdirection < 0:
+		animated_sprite.flip_h = true
+
+# Second way of doing movement animation
+	#if Xdirection != 0 or Ydirection != 0:
+	#		animated_sprite.play("Run")
+	#else:
+	#	animated_sprite.play("Idle")
+	
+	if Xdirection == 0 and Ydirection == 0:
+		animated_sprite.play("Idle")
+	else:
+		animated_sprite.play("Run")
